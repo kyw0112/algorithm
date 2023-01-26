c943d5a780c1ad84e17876851157c8c92a30b8e5
Kim Young Woo <109413998+kyw0112@users.noreply.github.com>
2023-01-26 11:28:11 +0900

    [Silver II] Title: 좌표 압축, Time: 1836 ms, Memory: 149684 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -0,0 +1,16 @@
+# 18870 좌표압축
+
+N = int(input())
+X = list(map(int, input().split()))
+
+A=X[:]
+# 세트로 중복 제거 후 리스트 형변환 -> sort ( 세트는 순서 없기 때문에 중복 제거 하고 소트)
+A=set(A)
+A=list(A)
+A.sort()
+B=[ i for i in range(len(A))]
+
+dic=dict(zip(A,B))
+
+for k in range(N):
+    print(dic[X[k]], end=' ')
