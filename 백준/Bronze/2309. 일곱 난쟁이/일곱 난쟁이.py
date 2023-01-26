13b8f1a702cbb8d61134bc0a3f8729f9899c0776
Kim Young Woo <109413998+kyw0112@users.noreply.github.com>
2023-01-26 09:41:18 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,3 +1,4 @@
+#2309 일곱난쟁이
 import sys
 input=sys.stdin.readline
 
@@ -10,22 +11,23 @@ for i in range(9):
 
     height_li.append(n)
 
-for j in range(9):
+for j in range(8):
 
 
     for k in range(j+1,9):
-
         if (sum(height_li)-height_li[j]-height_li[k])==100:
             height_li.remove(height_li[j])
-            height_li.remove(height_li[k-1]) # 하나 리무브 하고나면 인덱스 땡겨지는구나!
-            break
+            height_li.remove(height_li[k - 1])  # 하나 리무브 하고나면 인덱스 땡겨지는구나!
 
-    if (len(height_li)==7) :
-        break # 브레이크로 반복문 탈출하면 for문의 j, k 등 변수가 지역변수마냥 사라져버리더라
+            break
+    if (len(height_li) == 7):
+        break
+
+
+
 
 height_li.sort()
 
 for p in range(len(height_li)):
     print(height_li[p])
-
 
