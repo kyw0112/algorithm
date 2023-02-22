6ddd0b8dc17c59526319dfbbc8b77cbe003c6399
Kim Young Woo <109413998+kyw0112@users.noreply.github.com>
2023-02-22 11:52:00 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 328 ms, Memory: 115396 KB -BaekjoonHub

--- 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
@@ -6,15 +6,11 @@ N, P = map(int, sys.stdin.readline().rstrip().split())
 finger_cnt = 0
 for _ in range(N):
     i, j = map(int, sys.stdin.readline().rstrip().split()) # 음 프랫
-
-    if stack[i]:
-        while stack[i] and stack[i][-1]>j:
-            stack[i].pop()
-            finger_cnt += 1
-
-        if stack[i] and stack[i][-1]==j:
-            continue
-
+    while stack[i] and stack[i][-1]>j:
+        stack[i].pop()
+        finger_cnt += 1
+    if stack[i] and stack[i][-1]==j:
+        continue
     stack[i].append(j)
     finger_cnt += 1
 print(finger_cnt)
