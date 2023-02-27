b5f74da98fdece3659c1e8a0fdadb390085aec7f
Kim Young Woo <109413998+kyw0112@users.noreply.github.com>
2023-02-27 23:42:30 +0900

    [Silver II] Title: 최대 힙, Time: 228 ms, Memory: 120448 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,19 @@
+from heapq import heappop, heappush
+import sys
+heap = []
+
+
+N = int(sys.stdin.readline().rstrip())
+
+for _ in range(N):
+
+    n =int(sys.stdin.readline().rstrip())
+
+    if n==0:
+        if len(heap)==0:
+            print(0)
+        else:
+            print(heappop(heap)[1])
+
+    else:
+        heappush(heap, (-n, n))
